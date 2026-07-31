37190b49be42de4b4fdfaa84f57b91de009fc0dd
Aidar Amrekulov <a.amrekulov@gmail.com>
2026-08-01 00:51:14 +0100

    debug(analytics): log all fbq event calls to console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              {if(f.fbq)return;n=f.fbq=function(){console.log('[Meta Pixel Event Call]', Array.from(arguments));n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
               n.queue=[];t=b.createElement(e);t.async=!0;
