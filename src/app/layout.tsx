1f762c016e5f3b7bce94bd660a1e95e0b47f1a1a
Aidar Amrekulov <a.amrekulov@gmail.com>
2026-08-01 00:01:00 +0100

    fix(analytics): pass autoConfig false in fbq init options object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('set', 'autoConfig', false, '320483099619378');
-              fbq('init', '320483099619378');
+              fbq('init', '320483099619378', {}, { autoConfig: false });
               fbq('track', 'PageView');
             `,
           }}
